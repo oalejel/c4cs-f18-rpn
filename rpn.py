fbf91da024e77ff913a9139b791ae9998123f9aa
oalejel <alejel.omar@gmail.com>
2018-11-07 15:51:04 -0500

    provided solution to exponentiation

--- rpn.py
+++ rpn.py
@@ -21,6 +21,8 @@ def calculate(arg):
                 result = val1 - val2
             elif token == '*':
                 result = val1 * val2
+            elif token == '^':
+                result = val1 ** val2
 
             stack.append(result)
             if len(stack) > 1:
